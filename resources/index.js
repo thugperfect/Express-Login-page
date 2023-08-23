520505d8aa9f5601fd0ad09354a2135040368147
thugperfect <meenamani9444@gmail.com>
2023-08-23 16:29:23 +0530

    validation in vanilla js

--- resources/index.js
+++ resources/index.js
@@ -1,19 +1,29 @@
 
+
 const uname = document.getElementById('uname')
 const passwd = document.getElementById("passwd")
 const submit = document.getElementById("submit-btn")
 
-const fetchData =async()=>{
-    const data  = await fetch("/api/users")
-    const usersData = await data.json()
-    return usersData
-}
-const find =() =>{
-    submit.addEventListener("click",(e)=>{
+const fetchData =()=>{
+    submit.addEventListener("click",async(e)=>{
         e.preventDefault()
-    
-        
+        const data  = await fetch("/api/users")
+        const usersData = await data.json()
+        let flag = false
+      
+        usersData.map((prop)=>{
+      
+          if(prop.uname === uname.value && prop.passwd === uname.passwd)  {
+           console.log(true);
+          }
+          
+        })
     })
+   
+}
+fetchData()
+const find =async() =>{
+  console.log(usersData);
 }
 
 
