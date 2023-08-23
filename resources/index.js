e31a2bebfb0949983adff30f335dea73521406a9
thugperfect <meenamani9444@gmail.com>
2023-08-23 16:12:08 +0530

    fetched data from /api/users

--- resources/index.js
+++ resources/index.js
@@ -1,13 +1,18 @@
 
-
-const uname = document.getElementById('uname').value
-const passwd = document.getElementById("passwd").value
+const uname = document.getElementById('uname')
+const passwd = document.getElementById("passwd")
 const submit = document.getElementById("submit-btn")
 
+const fetchData =async()=>{
+    const data  = await fetch("/api/users")
+    const usersData = await data.json()
+    return usersData
+}
 const find =() =>{
     submit.addEventListener("click",(e)=>{
         e.preventDefault()
-        console.log(uname,passwd);
+    
+        
     })
 }
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const { data }= require('./data')
+const { stringify } = require('querystring')
 app.use(express.static('./resources'))
 app.use(express.urlencoded({extended:false}))
 
@@ -10,15 +11,19 @@ app.get('/api/users',(req,res)=>{
 })
 
 app.post('/login',(req,res)=>{
-    const request= req.body
+    const request = req.body
+    let flag = false
     data.map((prop)=>{
         if(prop.uname === request.uname && prop.passwd === request.passwd){
-            res.send("Login Successful")
+            flag = true
+            res.send("welcome")
         }
-        else{
-            res.send("Fuck up Buddy")
-        }
+       
     })
+    if(flag === false){
+        res.send("fuck up")
+    }
+
    
 })
 app.listen(5000,()=>{
